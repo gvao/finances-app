464605abb5df9baf1b6a197fcf5febf2fbd0743a
Yuri Galvao <yuri.gvao@gmail.com>
2023-10-22 16:51:58 -0300

    fix saveAccount use cases

--- src/core/account/use-cases/save-account.ts
+++ src/core/account/use-cases/save-account.ts
@@ -4,7 +4,7 @@ import { Repository } from "../../shared/repository";
 export function saveAccount(repository: Repository, { name, total }: Account) {
 	const account = CreateAccount({ name, total });
 
-	repository.save(account);
+	repository.add(account);
 
 	console.log(account);
 }
