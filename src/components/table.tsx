008ac0f83e31802b897029bc3e8a240df0a4d391
Yuri Galvao <yuri.gvao@gmail.com>
2023-10-22 16:15:11 -0300

    created table component

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,36 +1,61 @@
+import { useContext } from "react";
 import { Account } from "../core/account/model";
+import { AccountContext } from "../context/accounts";
 
-interface TableExpensesProps {
-  accounts: Array<Account>;
-}
-export const TableExpenses = ({ accounts }: TableExpensesProps) => {
-  if (accounts.length < 0) return null;
+export const TableExpenses = () => {
+	const { accounts } = useContext(AccountContext);
 
-  const blacklist = ["id"];
-  const headers = (
-    accounts.length > 0 ? (Object.keys(accounts[0]) as (keyof Account)[]) : []
-  ).filter((key) => !blacklist.includes(key));
+	if (accounts.length < 0) return null;
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          {headers.map((key, i) => (
-            <th key={`header-${key}-${i}`}>{key}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {accounts
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          .map((account: Record<string, any>) => (
-            <tr key={account.id}>
-              {headers.map((key) => (
-                <td key={key + "-" + account.id}>{account[key]}</td>
-              ))}
-            </tr>
-          ))}
-      </tbody>
-    </table>
-  );
+	const headers = ["name", "total"];
+
+	return (
+		<table>
+			<thead>
+				<tr>
+					{headers.map((key, i) => (
+						<th key={`header-${key}-${i}`}>{key}</th>
+					))}
+				</tr>
+			</thead>
+			<tbody>
+				{accounts
+					// eslint-disable-next-line @typescript-eslint/no-explicit-any
+					.map((account) => (
+						<TableRow
+							key={account.id}
+							account={account}
+							headers={headers}
+						/>
+					))}
+			</tbody>
+		</table>
+	);
+};
+
+const TableRow = ({
+	account,
+	headers,
+}: {
+	account: Account;
+	headers: string[];
+}) => {
+
+	const { deleteAccount } = useContext(AccountContext);
+
+	const deleteAccountOnClick = () => {
+		deleteAccount!(account.id!)
+	}
+
+	return (
+		<tr>
+			{headers.map((key) => (
+				<td key={key + "-" + account.id}>{account[key]}</td>
+			))}
+
+			<td>
+				<button onClick={deleteAccountOnClick}>delete</button>
+			</td>
+		</tr>
+	);
 };
