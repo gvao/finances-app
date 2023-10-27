d963ce9987739dc642a2788002284aa3cc6c76df
Yuri Galvao <yuri.gvao@gmail.com>
2023-10-27 18:13:27 -0300

    update summary style

--- src/components/month-summary/index.tsx
+++ src/components/month-summary/index.tsx
@@ -1,6 +1,5 @@
 import { useAccountContext } from "../../context/accounts";
 import { transformCurrency } from "../../utils";
-import Button from "../common/button";
 import styles from "./styles.module.css";
 
 export const MonthSummary = () => {
@@ -17,9 +16,9 @@ export const MonthSummary = () => {
 	return (
 		<div className={styles.wrap}>
 			<div className={styles.actions}>
-				<Button onClick={prevMonth}>prev</Button>
+				<ChevronLeft onClick={prevMonth} />
 				<h2>{labelMonth}</h2>
-				<Button onClick={nextMonth}>next</Button>
+				<ChevronRight onClick={nextMonth} />
 			</div>
 
 			<div>
@@ -30,3 +29,35 @@ export const MonthSummary = () => {
 		</div>
 	);
 };
+
+const ChevronLeft = ({ ...props }) => (
+	<svg
+		className={styles.icon}
+		{...props}
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		fill="currentColor"
+	>
+		<path
+			fillRule="evenodd"
+			d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z"
+			clipRule="evenodd"
+		/>
+	</svg>
+);
+
+const ChevronRight = ({ ...props }) => (
+	<svg
+		className={styles.icon}
+		{...props}
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		fill="currentColor"
+	>
+		<path
+			fillRule="evenodd"
+			d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
+			clipRule="evenodd"
+		/>
+	</svg>
+);
