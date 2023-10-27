7725c9c00807ca453d8b3851ad316382b46e362b
Yuri Galvao <yuri.gvao@gmail.com>
2023-10-27 15:04:17 -0300

    column date in tableAccounts

--- src/components/table-account/index.tsx
+++ src/components/table-account/index.tsx
@@ -11,9 +11,9 @@ export const TableExpenses = () => {
 		<table className={styles.table}>
 			<thead>
 				<tr>
-					<th>Titulo</th>
-					<th>Valor</th>
 					<th>Data</th>
+					<th>Descrição</th>
+					<th>Valor</th>
 					<th>actions</th>
 				</tr>
 			</thead>
@@ -28,33 +28,39 @@ export const TableExpenses = () => {
 
 function TableRow({ account, ...props }: { account: Account }) {
 	const { deleteAccount } = useAccountContext();
-
-	const { name, total, id, createAt } = account;
-	const date = new Date(createAt);
+	const { name, total, id, date } = account;
 
 	const deleteAccountOnClick = () => deleteAccount!(id!);
 
+	const dateFormatted = transformDate(date, {
+		day: "2-digit",
+		month: "short",
+	});
+
 	return (
-		<tr key="teste" {...props} className={styles.row}>
+		<tr {...props} className={styles.row}>
+			<td className={styles.cell}>{dateFormatted}</td>
 			<td className={styles.cell}>{name}</td>
 			<td className={styles.cell}>R$ {transformCurrency(total)}</td>
-			<td className={styles.cell}>{transformDate(date)}</td>
 
 			<td className={styles.cell}>
-				<svg
-					// className={} //insert style
-					onClick={deleteAccountOnClick}
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="currentColor"
-				>
-					<path
-						fillRule="evenodd"
-						d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
-						clipRule="evenodd"
-					/>
-				</svg>
+				<TrashIcon onClick={deleteAccountOnClick} />
 			</td>
 		</tr>
 	);
 }
+
+const TrashIcon = ({ ...props }) => (
+	<svg
+		{...props}
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		fill="currentColor"
+	>
+		<path
+			fillRule="evenodd"
+			d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+			clipRule="evenodd"
+		/>
+	</svg>
+);
