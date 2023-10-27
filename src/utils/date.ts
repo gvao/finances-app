5ce223c9b1d709669a7a6da2cd6cbbd6330c70b2
Yuri Galvao <yuri.gvao@gmail.com>
2023-10-27 15:32:04 -0300

    fix formatDate receive string or Date in params

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,11 @@
-export const transformDate = (
-	date: Date,
+export const FormatDate = (
+	date?: Date | string,
 	options: Intl.DateTimeFormatOptions | undefined = {},
 	locales = "pt-Br"
 ) => {
   if(!date) return null
+
+  if(typeof date === 'string') date = new Date(date)
 
   return new Intl.DateTimeFormat(locales, options).format(new Date(date));
 }
