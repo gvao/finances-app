53f10da86dc9ace8e0a10eba35799eb06b739b70
Yuri Galvao <yuri.gvao@gmail.com>
2024-06-16 23:01:44 -0300

    update Item style

--- src/modules/shared/component/Item/index.tsx
+++ src/modules/shared/component/Item/index.tsx
@@ -1,4 +1,8 @@
+import { LiHTMLAttributes } from "react";
 import { ChildrenProps } from "../../types/children";
 import styles from "./styles.module.css"
 
-export const Item = ({ children }: ChildrenProps) => <li className={styles.wrap}>{children}</li>
+export const Item = ({ children, className, ...props }: Props) =>
+    <li className={`${className} ${styles.wrap}`} {...props}>{children}</li>
+
+type Props = ChildrenProps & LiHTMLAttributes<HTMLLIElement>
